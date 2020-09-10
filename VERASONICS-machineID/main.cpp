8d62dc2642f2d7b952e43e164e1815111e31cc43
daniele_trimarchi <daniele.trimarchi@iconeus.com>
2020-09-10 18:23:24 +0200

    Add a finction getboardName from VERASONICS\dev-apps\c-hwdiag-app\src\parseArgs.c Where is this sucker normally stored? Shouldn't this be into a lib ?

--- VERASONICS-machineID/main.cpp
+++ VERASONICS-machineID/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+
+#include <VH_Hardware.h>
 #include <VC_Threads.h>
+
+#include <VH_PciSwitchRegisters.h>
+#include <parseArgs.h>
 #include <VH_HardwarePrototypes.h>
+
+char * BoardNames[] = {"TPC", "SHI", "BKP", "ACQ1", "ACQ2", "ACQ3", "ACQ4", "UTA", "ALL"};
+
+enum VH_BoardId getBoardFromName(const char *boardName)
+{
+    int result = -1;
+    int i, numBoards;
+
+    if(boardName != NULL)
+    {
+        numBoards = VH_MAX_NUM_BOARDS;
+        for(i = 0; i < numBoards; i++)
+        {
+            if(strcasecmp(boardName, BoardNames[i]) == 0)
+            {
+                result = i;
+                break;
+            }
+        }
+    }
+    return result;
+}
 
 int main() {
 
@@ -9,7 +37,27 @@ int main() {
     // it lives in HAL-NG 'C' Shared Library." : System/libVerasonicsHal-NG.dll
     //                                           System/   VerasonicsHal-NG.lib
     enum VH_HardwareOpenResult hardwareOpenResult;
-    hardwareOpenResult = VH_OpenHardware(FALSE); 
-    
-    return 0;
+    hardwareOpenResult = VH_OpenHardware(FALSE);  // false means read-only, we are only doing queries
+		
+	// Generate our status string
+	std::string status( VH_GetHardwareOpenResultAsString(hardwareOpenResult) );
+	std::cout << "VERASONICS HARDWARE OPEN STATUS: \n\t" << status << std::endl;
+	
+	switch(hardwareOpenResult){
+		case Vh_HardwareOpenResultSuccess :
+			std::cout<<"Hardware open Success!";
+			enum VH_BoardId boardId;
+			boardId = getBoardFromName("SHI");
+			
+			break;
+		
+		case Vh_HardwareOpenResultFailedBecauseHardwareAdapterNotActivated : 
+			std::cout<<"Hardware not activated!";
+			break; 		
+		default: 
+			std::cout<<"Other case";
+	}
+	
+	return 0;
 }
+
